febd0845cb018c62b7adcd07dcd982b100797f20
Akhil Siraswal <akhilsiraswal@gmail.com>
2020-11-01 10:39:58 +0530

    1-11-20

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-// var modelBtn = document.querySelector(".btn");
-// console.log(modelBtn);
-// var modelBg = document.querySelector(".modal-bg");
-// console.log(modelBg);
-// var modalClose = document.querySelector(".modal-close");
-// console.log(modelClose);
-// modelBtn.addEventListener("click", function () {
-//   console.log("Inside click");
-//   modelBg.classList.add("bg-active");
-// });
-// console.log("Inside app.js");
-// modalClose.addEventListener("click", function () {
-//   modelBg.classList.remove("bg-active");
-// });
-
 const texts = [
   "Security Appliances",
   "Computer Repair",
